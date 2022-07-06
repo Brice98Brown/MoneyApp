e53be36fa04f247d79090b9e78d9eb487c950511
Jessi Austin <jessimasmith.421@gmail.com>
2022-07-06 12:22:00 -0400

    Made Account Model,DAO, and GetAccountByAccountId method in DAO

---
+++ TenmoServer/DAO/AccountDAO.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TenmoServer.Models;
+using System.Data.SqlClient;
+
+namespace TenmoServer.DAO
+{
+    public class AccountDAO : IAccountDAO
+    {
+        private readonly string connectionString;
+        public AccountDAO (string connstr)
+        {
+            this.connectionString = connstr;
+        }
+        public Account GetAccountByAccountId(int accountId)
+        {
+            string query = "SELECT account_id,user_id,balance FROM accounts WHERE user_id = @id";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@id", accountId);
+
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    Account account = GetAccountFromDataReader(reader);
+                    return account;
+                }
+            }
+            return null;
+        }
+
+        private Account GetAccountFromDataReader(SqlDataReader reader)
+        {
+            int userId = Convert.ToInt32(reader["user_id"]);
+            int accountNumber = Convert.ToInt32(reader["account_id"]);
+            decimal balance = Convert.ToDecimal(reader["balance"]);
+
+            Account account = new Account();
+            account.AccountId = accountNumber;
+            account.UserId = userId;
+            account.Balance = balance;
+
+            return account;
+        }
+    }
+}
--- TenmoServer/Models/ReturnUser.cs
+++ TenmoServer/Models/ReturnUser.cs
@@ -7,7 +7,7 @@
     {
         public int UserId { get; set; }
         public string Username { get; set; }
-        //public string Role { get; set; }
+        //public string Role { get; set; } this app has no roles, so we don't need to worry about this
         public string Token { get; set; }
     }
 }
